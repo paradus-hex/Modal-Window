20f3946852286394e3d7a4b36082bd2816a9aebf
hex-paradus <paradushex@gmail.com>
2021-08-11 04:07:06 +0600

    Modal Window

---
+++ script.js
@@ -0,0 +1,21 @@
+'use strict';
+const btnsShowModal = document.querySelectorAll('.show-modal'); //Creating variable so that i don't have type the whole thing like in the first project
+//querySelectorALL selects multiple elements inside a class and presents it in basically an array[called node]
+const btnCloseModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden'); //notice there is no "." before hidden as it's not an element to be selected.removing the hidden property brings the modal window to light
+  overlay.classList.remove('hidden'); //brings the blurr to the front
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener('click', openModal);
+}
+btnCloseModal.addEventListener('click', closeModal); //notice that the function isn't called with ()i.e closeModal().That is because, doing so calls the function immediately to run. but with the bracs the function will be auto run by the event handler when the button is clicked
+overlay.addEventListener('click', closeModal);
